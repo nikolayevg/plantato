188ea70288ecaa2f22db3fd5f326b1c01811d0db
Nik <n.e.lobanov@gmail.com>
2025-07-13 18:17:57 +0300

    improved path in app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
 # Конфигурация
-MODEL_ID = '/home/nick/deeplearning/DIPLOMA/project/plantato/checkpoints/var_2'
+MODEL_ID = '/checkpoints/var_2' # путь до папки с чекпоинтом (лучше указать полный)
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 @st.cache_resource
